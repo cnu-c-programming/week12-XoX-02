c62920d614ad3932aceb75cf99fdfc57fa467929
XoX-02 <boy3898@naver.com>
2026-05-22 16:43:08 +0900

    update

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,9 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-    while(1){
-        int n = fscanf(fp,"%s %d",students[count].name, &students[count].score);
-        if(n == 2){
+    while(fscanf(fp,"%s %d",students[count].name, &students[count].score) == 2){
             count ++;
             if(feof(fp)) break;
-        }
     }
 
     int max = 0;
